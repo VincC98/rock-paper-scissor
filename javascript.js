5587d0cd3c3616eb546ea43d0d18cb9b44d2cc4d
VincC98 <cammaranovincent98@gmail.com>
2025-03-01 16:05:58 +0100

    added UI

--- javascript.js
+++ javascript.js
@@ -11,42 +11,101 @@ function getComputerChoice(){
     else return "scissor";
 }
 
+function playRound(humanChoice){
+    console.log("human choice is: " + humanChoice)
+    let computerChoice = getComputerChoice();
+    console.log("computer choice is: " + computerChoice);
 
-
-function getHumanChoice(){
-    let choice = prompt("Enter your choice for rock paper scissor");
-    return choice.toLocaleLowerCase();
-}
-
-
-
-function playRound(humanChoice, computerChoice){
     switch (humanChoice) {
         case 'paper':
             if (computerChoice == 'rock'){
                 humanScore += 1;
+                result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
+                if (humanScore == 5){
+                    result.innerHTML = "The winner is Human!";
+                    humanScore = 0;
+                    computerScore = 0;
+                }
+                else if (computerScore == 5) {
+                    result.innerHTML = "The winner is AI!";
+                    humanScore = 0;
+                    computerScore = 0;
+                }
                 return "You win! Paper beats Rock";
             }
             else {
                 computerScore +=1;
+                result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
+                if (humanScore == 5){
+                    result.innerHTML = "The winner is Human!";
+                    }
+                    else if (computerScore == 5) {
+                        result.innerHTML = "The winner is AI!";
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
                 return "You loose! Scissor beats Paper";
             }
         case 'rock':
             if (computerChoice == 'scissor'){
                 humanScore += 1;
+                result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
+                if (humanScore == 5){
+                    result.innerHTML = "The winner is Human!";
+                    humanScore = 0;
+                    computerScore = 0;
+                    }
+                    else if (computerScore == 5) {
+                        result.innerHTML = "The winner is AI!";
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
                 return "You win! Rock beats Scissor";
             }
             else {
                 computerScore +=1;
+                result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
+                if (humanScore == 5){
+                    result.innerHTML = "The winner is Human!";
+                    humanScore = 0;
+                    computerScore = 0;
+                    }
+                    else if (computerScore == 5) {
+                        result.innerHTML = "The winner is AI!";
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
                 return "You loose! Paper beats Rock";
             }
         case 'scissor':
             if (computerChoice == 'rock'){
                 humanScore += 1;
+                result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
+                if (humanScore == 5){
+                    result.innerHTML = "The winner is Human!";
+                    humanScore = 0;
+                    computerScore = 0;
+                    }
+                    else if (computerScore == 5) {
+                        result.innerHTML = "The winner is AI!";
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
                 return "You win! Scissor beats Paper";
             }
             else {
                 computerScore +=1;
+                result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
+                if (humanScore == 5){
+                    result.innerHTML = "The winner is Human!";
+                    humanScore = 0;
+                    computerScore = 0;
+                    }
+                    else if (computerScore == 5) {
+                        result.innerHTML = "The winner is AI!";
+                        humanScore = 0;
+                        computerScore = 0;
+                    }
                 return "You loose! Rock beats Scissor";
             }
         default:
@@ -54,7 +113,14 @@ function playRound(humanChoice, computerChoice){
     }
 }
 
-let computerChoice = getComputerChoice();
-let humanChoice = getHumanChoice();
+const buttons = document.querySelectorAll("button")
+console.log(buttons)
+buttons.forEach(button => {
+    button.addEventListener("click", function (event) {
+        console.log(event.target.innerText);
+        console.log(playRound(event.target.innerText.toLowerCase()));
+      });
+});
 
-console.log(playRound(humanChoice, computerChoice));
+const result = document.querySelector("#results")
+result.innerHTML = "Current score is: " + humanScore + " for human / " + computerScore + " for the AI";
